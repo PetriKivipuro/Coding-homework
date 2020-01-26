60988e082d16c31e9f438c048cd1052d32e064f1
Petri <petri.kivipuro@gmail.com>
2020-01-26 16:20:09 +0200

    Exercise 32 done

--- part1/conditionals/exercise_32/Program.cs
+++ part1/conditionals/exercise_32/Program.cs
@@ -7,6 +7,19 @@ namespace exercise_32
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine("Speak, friend, and enter!");
+      string speak = Console.ReadLine();
+      
+      if (speak == "Mellon") 
+      {
+        Console.WriteLine("Welcome, friend!");
+      }
+
+      else
+      {
+          Console.WriteLine("They've got a cave troll!");
+      }
+
 
     }
   }
