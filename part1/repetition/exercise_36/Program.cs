fb3cee7bf8a281edbda439d4bcc487f203ccb7d2
Petri <petri.kivipuro@gmail.com>
2020-01-26 19:08:01 +0200

    Exercise 36 done

--- part1/repetition/exercise_36/Program.cs
+++ part1/repetition/exercise_36/Program.cs
@@ -8,6 +8,27 @@ namespace exercise_36
     {
       // Write your code here:
 
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        int numb1 = Convert.ToInt32(Console.ReadLine());
+
+        if (numb1 == 0)
+        {
+          break;
+        }
+        
+        else if (numb1 < 0)
+        {
+          Console.WriteLine("That is negative");
+        }
+
+        else if (numb1 > 0)
+        {
+          Console.WriteLine(Math.Pow(numb1, 2));
+        }
+      }
+
     }
   }
 }
