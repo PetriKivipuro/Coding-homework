3b5fa03f116a93159667151673108c0fc65abe33
Petri <petri.kivipuro@gmail.com>
2020-01-27 15:26:40 +0200

    Exercise 42 done

--- part2/subproblems/exercise_42/Program.cs
+++ part2/subproblems/exercise_42/Program.cs
@@ -8,6 +8,13 @@ namespace exercise_42
     {
 
       // Write your code here:
+    int first = Convert.ToInt32(Console.ReadLine());
+    int second = Convert.ToInt32(Console.ReadLine());
+
+    int sum = (first + second);
+
+    double squareRoot = Math.Sqrt(sum);
+    Console.WriteLine(squareRoot);
     
     }
   }
