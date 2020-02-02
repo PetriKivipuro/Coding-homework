29f01eee1117ffe7278872cb723f4ef9a88649e9
Petri <petri.kivipuro@gmail.com>
2020-02-02 15:40:51 +0200

    Exercises 54 and 55 done

--- part2/methods/exercise_55/Program.cs
+++ part2/methods/exercise_55/Program.cs
@@ -2,14 +2,21 @@
 
 namespace exercise_55
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
+        public static void Main(String[] args)
+        {
+            int number = NumberUno();
+            Console.WriteLine(number);
+        }
+
+        // Write your method here:
+
+        public static int NumberUno()
+        {
+            return 1;
+        }
+
 
     }
-
-    // Write your method here:
-
-  }
 }
