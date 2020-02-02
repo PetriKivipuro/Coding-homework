608d1c71904cf9f08f49a14d48072bad3f57d113
Petri <petri.kivipuro@gmail.com>
2020-02-02 17:13:43 +0200

    Exercise 59 done

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -2,15 +2,33 @@
 
 namespace exercise_59
 {
-  class Program
-  {
-    public static void Main(String[] args)
+    class Program
     {
-      int answer = Greatest(2, 7, 3);
-      Console.WriteLine("Greatest: " + answer);
+        public static void Main(String[] args)
+        {
+            int answer = Greatest(2, 7, 3);
+            Console.WriteLine("Greatest: " + answer);
+        }
+
+        // Write your method here:
+        public static int Greatest(int number1, int number2, int number3)
+        {
+            if (number1 > number2 & number1 > number3)
+            {
+                return number1;
+            }
+
+            if (number2 > number1 & number2 > number3)
+            {
+                return number2;
+            }
+
+            else
+            {
+                return number3;
+            }
+
+
+        }
     }
-
-    // Write your method here:
-
-  }
 }
