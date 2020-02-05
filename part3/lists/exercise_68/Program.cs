cc3f2e3cebf342b21863728007a9c1ea9253f988
Petri <petri.kivipuro@gmail.com>
2020-02-05 14:25:16 +0200

    exercise 68 done

--- part3/lists/exercise_68/Program.cs
+++ part3/lists/exercise_68/Program.cs
@@ -3,21 +3,32 @@ using System.Collections.Generic;
 
 namespace exercise_68
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
+        public static void Main(string[] args)
         {
-          break;
-        }
-        list.Add(input);
-      }
 
+            List<int> list = new List<int>();
+            while (true)
+            {
+
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input == -1)
+                {
+                    break;
+                }
+                list.Add(input);
+
+
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+
+
+
+        }
     }
-  }
 }
