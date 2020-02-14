c390c37c3f1938321396e483b09ac251a4361898
Petri <petri.kivipuro@gmail.com>
2020-02-14 10:40:56 +0200

    Exercise 82 done

--- part3/lists/exercise_74/Program.cs
+++ part3/lists/exercise_74/Program.cs
@@ -3,40 +3,38 @@ using System.Collections.Generic;
 
 namespace exercise_74
 {
-    class Program
+  class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+      List<string> list = new List<string>();
+      while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
         {
-            List<string> list = new List<string>();
-            while (true)
-            {
-                string input = Console.ReadLine();
-                if (input == "")
-                {
-                    break;
-                }
-                list.Add(input);
-
-
-
-            }
-            Console.WriteLine("Search for?");
-            string whoseLost = Console.ReadLine();
-            //Console.WriteLine(" --- u gave " + whoseLost); //debugg
-            // Debugg  --- Console.WriteLine(list.Contains(whoseLost));
-
-            //Tarkistaa sisältääkö "list" elementin arvolla whoseLost
-            if (list.Contains(whoseLost))
-            {
-                Console.WriteLine(whoseLost + " was found!");
-            }
-            else //id (!/list.Contains(whoseLost)) Kävisi myös tähän, mutta else on parempi, koska vain kaksi vaihtoehtoa.
-            {
-                Console.WriteLine(whoseLost + " was not found!");
-            }
-
-
+          break;
         }
+        list.Add(input);
+
+      }
+      Console.WriteLine("Search for?");
+      string whoseLost = Console.ReadLine();
+      //Console.WriteLine(" --- u gave " + whoseLost); //debugg
+      // Debugg  --- Console.WriteLine(list.Contains(whoseLost));
+
+      //Tarkistaa sisältääkö "list" elementin arvolla whoseLost
+      if (list.Contains(whoseLost))
+      {
+        Console.WriteLine(whoseLost + " was found!");
+      }
+      else //id (!/list.Contains(whoseLost)) Kävisi myös tähän, mutta else on parempi, koska vain kaksi vaihtoehtoa.
+      {
+        Console.WriteLine(whoseLost + " was not found!");
+      }
+
+
     }
+  }
 }
 
