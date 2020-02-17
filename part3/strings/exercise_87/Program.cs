f4d491a9a20079e946bdc611b9c4861b4ab24424
Petri <petri.kivipuro@gmail.com>
2020-02-17 13:09:20 +0200

    Exercise 87 done

--- part3/strings/exercise_87/Program.cs
+++ part3/strings/exercise_87/Program.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 
 namespace exercise_87
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                string words = Console.ReadLine();
+                string[] lines = words.Split(" ");
+                if (words == "")
+                {
+                    break;
+                }
+
+                for (int i = 0; i < lines.Length; i++)
+                    if (lines[i].Contains("av"))
+                    {
+                        Console.WriteLine(lines[i]);
+
+                    }
+
+
+
+
+            }
+        }
 
     }
-
-  }
 
 }
 
