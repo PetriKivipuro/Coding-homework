7239a1a07dffac5228b17389ac83f0d47712c152
Petri <petri.kivipuro@gmail.com>
2020-02-18 21:08:52 +0200

    Obejct oriented programming material

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
+
 namespace sandbox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person ada = new Person("Ada");
+            Person antti = new Person("Antti");
+            Person martin = new Person("Martin");
+
+            ada.PrintPerson();
+            antti.PrintPerson();
+            martin.PrintPerson();
+
+            Console.WriteLine();
+
+            ada.GrowOlder();
+            antti.GrowOlder();
+            antti.GrowOlder();
+            antti.GrowOlder();
+            antti.GrowOlder();
+            antti.GrowOlder();
+            martin.GrowOlder();
+
+            ada.PrintPerson();
+            antti.PrintPerson();
+            martin.PrintPerson();
+
+            // selvennykseksi kaksi tyhjää riviä
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+
+            Teacher teacher = new Teacher();
+            int grading = teacher.Grade();
+
+            Console.WriteLine("The grade is recieved is " + grading);
+
+            // selvennykseksi kaksi tyhjää riviä
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------");
+
+            Teacher firts = new Teacher();
+            Teacher second = new Teacher();
+            Teacher third = new Teacher();
+
+            double average = (firts.Grade() + second.Grade() + third.Grade()) / 3.0;
+            Console.WriteLine("The grading average " + average);
+
         }
+
     }
 }
