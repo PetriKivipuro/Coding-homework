06248051139af511965d10a6421b174a3153355a
Petri <petri.kivipuro@gmail.com>
2020-02-20 22:16:51 +0200

    100 kesken

--- part4/objects/exercise_100/Program.cs
+++ part4/objects/exercise_100/Program.cs
@@ -2,13 +2,26 @@
 
 namespace exercise_100
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
+            Debt mortgage = new Debt(120000.0, 1.01);
 
+            mortgage.PrintBalance();
+            mortgage.WaitOneYear();
+
+            int year = 0;
+            while (year < 20)
+            {
+                mortgage.WaitOneYear();
+                year = year + 1;
+
+            }
+            mortgage.PrintBalance();
+
+        }
     }
-  }
 }
 
 
