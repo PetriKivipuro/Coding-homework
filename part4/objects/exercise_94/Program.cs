87e34226780c6b1a5ab5db17f9c5780a280f2603
Petri <petri.kivipuro@gmail.com>
2020-02-20 16:00:30 +0200

    Exercise 94 done

--- part4/objects/exercise_94/Program.cs
+++ part4/objects/exercise_94/Program.cs
@@ -8,6 +8,14 @@ namespace exercise_94
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account heikinTili = new Account("Heikki's account", 1000);
+      Account personalTili = new Account("Personal account", 0);
+
+      heikinTili.Withdrawal(100);
+      personalTili.Deposit(100);
+
+      Console.WriteLine(heikinTili.ToString());
+      Console.WriteLine(personalTili);
 
     }
   }
