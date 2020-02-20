a3663f13e91785c5c69404c41c66cecb43d5b205
Petri <petri.kivipuro@gmail.com>
2020-02-20 20:04:58 +0200

    Exercise 99 done

--- part4/objects/exercise_99/Program.cs
+++ part4/objects/exercise_99/Program.cs
@@ -2,18 +2,25 @@
 
 namespace exercise_99
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
+        public static void Main(string[] args)
+        {
 
-      DecreasingCounter counter = new DecreasingCounter(20);
-      counter.PrintValue();
+            DecreasingCounter counter = new DecreasingCounter(20);
+            counter.PrintValue();
 
-      counter.Reset();
-      counter.PrintValue();
+            counter.Decrement();
+            counter.PrintValue();
+            counter.Decrement();
+
+
+            counter.Reset();
+            counter.PrintValue();
+
+
+        }
     }
-  }
 }
 
 
