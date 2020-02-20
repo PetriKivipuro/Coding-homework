fa05bbe224fd15e7ab288dbffaf144d2d2fa20a4
Petri <petri.kivipuro@gmail.com>
2020-02-20 15:50:46 +0200

    Exercise 93 done

--- part4/objects/exercise_93/Program.cs
+++ part4/objects/exercise_93/Program.cs
@@ -8,6 +8,11 @@ namespace exercise_93
     {
       // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
+      Account petenTili = new Account("Peten tili", 100);
+
+      petenTili.Deposit(20);
+
+      Console.WriteLine(petenTili.balance);
 
     }
   }
