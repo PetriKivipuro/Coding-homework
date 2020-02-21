7f30408845a544141ef32d82e362adc9b12064fd
Petri <petri.kivipuro@gmail.com>
2020-02-21 09:59:05 +0200

    Exercise 100 done

--- part4/objects/exercise_100/Debt.cs
+++ part4/objects/exercise_100/Debt.cs
@@ -2,28 +2,28 @@ using System;
 
 namespace exercise_100
 {
-    class Debt
+  class Debt
+  {
+    private double balance;
+    private double interestRate;
+
+
+    public Debt(double initialBalance, double initialInterestRate)
     {
-        private double balance;
-        private double interestRate;
+      this.balance = initialBalance;
+      this.interestRate = initialInterestRate;
+    }
 
+    public void PrintBalance()
+    {
+      Console.WriteLine(this.balance);
+    }
+    public void WaitOneYear()
+    {
+      this.balance = (this.balance * this.interestRate);
 
-        public Debt(double initialBalance, double initialInterestRate)
-        {
-            this.balance = initialBalance;
-            this.interestRate = initialInterestRate;
-        }
-
-        public void PrintBalance()
-        {
-            Console.WriteLine(this.balance);
-        }
-        public void WaitOneYear()
-        {
-            double balanceAfetryr = (this.balance * this.interestRate);
-
-            Console.WriteLine(balanceAfetryr);
-        }
 
     }
+
+  }
 }
