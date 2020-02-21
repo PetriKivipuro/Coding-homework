3ea70e449ff628baa0c04e1e2ee0893bab609d68
Petri <petri.kivipuro@gmail.com>
2020-02-21 12:49:27 +0200

    Exercise 104 done

---
+++ part4/objects/exercise_104/Multiplier.cs
@@ -0,0 +1,17 @@
+using System;
+namespace exercise_104
+{
+  class Multiplier
+  {
+    private int number;
+
+    public Multiplier(int number12)
+    {
+      this.number = number12;
+    }
+    public int Multiply(int number666)
+    {
+      return this.number = this.number * number666;
+    }
+  }
+}
