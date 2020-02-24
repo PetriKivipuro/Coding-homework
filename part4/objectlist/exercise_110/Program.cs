298e1c385158ff8cf72bab88cdd12e0176a340d5
Petri <petri.kivipuro@gmail.com>
2020-02-24 11:32:08 +0200

    110 kesken

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 
 namespace exercise_110
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // IMPLEMENT YOUR CODE IN HERE!
+        public static void Main(string[] args)
+        {
+            // IMPLEMENT YOUR CODE IN HERE!
+            List<Item> items = new List<Item>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+
+                if (name == "")
+                {
+                    break;
+                }
+                items.Add(new Item(name));
+
+            }
+            foreach (Item item in items)
+            {
+                Console.WriteLine("Name: ");
+                Console.WriteLine(item);
+            }
+
+        }
+
     }
-  }
 }
 
 
