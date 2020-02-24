cdf668ab821ad300b02747efcb885cdf7e2f5c75
Petri <petri.kivipuro@gmail.com>
2020-02-24 13:23:24 +0200

    Exercise 110 done

--- part4/objectlist/exercise_110/Program.cs
+++ part4/objectlist/exercise_110/Program.cs
@@ -8,9 +8,39 @@ namespace exercise_110
     public static void Main(string[] args)
     {
       // IMPLEMENT YOUR CODE IN HERE!
+      List<Item> items = new List<Item>();
+
+      while (true)
+      {
+        // ohjelma pyytää ekaksi Name:
+        // sen perään käyttäjä antaa inputin
+        Console.Write("Name: ");
+        string input = Console.ReadLine();
+        // jos input on tyhjä -- ohjelma lopettaa
+        if (input == "")
+        {
+          break;
+        }
+        // jos nimi annetaan se lisätään listaan
+        items.Add(new Item(input));
+      }
+      // kun uusia inputteja eli nimiä ei enää tule 
+      // ohjelma lisää yhden tyhjän rivin
+      Console.WriteLine();
+      // ja sen jälkeen luokasta Item haetaan lista items
+      // items listalle annetaan nimi i
+      // sen jälkeen pyydetään ohjelmaa kirjoittamaan rivi
+      // jossa on käytätty i arvoa. 
+      // i palauttaa mainiin inputnimen sekä ajan jolloin se on luotu
+      foreach (Item i in items)
+      {
+        Console.WriteLine(i);
+      }
     }
   }
 }
+
+
 
 
 
