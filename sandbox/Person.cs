9ac565e4a9d47375ead955c650dc7908a4dcc6f6
Petri <petri.kivipuro@gmail.com>
2020-02-25 13:06:18 +0200

    sandbox treeni

--- sandbox/Person.cs
+++ sandbox/Person.cs
@@ -1,46 +1,13 @@
 using System;
 
-namespace person
+
+namespace sandbox
 {
-  public class Person
+  class Person
   {
-    private string name;
-    private int age;
-    private int weight;
-    private int height;
-
-
-    public Person(string name)
+    public Person(string name, int age)
     {
-      this.name = name;
-      this.age = 0;
-      this.weight = 0;
-      this.height = 0;
-    }
 
-    public double BodyMassIndex()
-    {
-      double heigthPerHundred = this.height / 100.0;
-      return this.weight / (heigthPerHundred * heigthPerHundred);
-    }
-
-    public bool IsAdult()
-    {
-      if (this.age < 18)
-      {
-        return false;
-      }
-      return true;
-    }
-
-    public void GrowOlder()
-    {
-      this.age++;
-    }
-
-    public override string ToString()
-    {
-      return this.name + ", age: " + this.age;
     }
   }
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,48 +1,78 @@
 ﻿using System;
-using person;
+using System.Collections.Generic;
+using System.IO;
+
 
 
 namespace sandbox
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      string text = File.ReadAllText("text.txt");
+
+      Console.WriteLine(" Tehty ReadAllText :lla!");
+      Console.WriteLine(text);
+
+      Console.WriteLine("****************************************");
+      Console.WriteLine();
+      Console.WriteLine("****************************************");
+
+      string[] lines = File.ReadAllLines("text.txt");
+
+      Console.WriteLine(" Tehty ReadAllLines :lla!");
+
+      foreach (string line in lines)
+      {
+        Console.WriteLine(line);
+      }
+      Console.WriteLine("****************************************");
+      Console.WriteLine();
+      Console.WriteLine("****************************************");
+
+
+      while (true)
+
+      {
+        Console.WriteLine("lisää nimi ja ikä. erota ne pilkulla");
+        string input = Console.ReadLine();
+        if (input == "")
         {
-            // Teacher first = new Teacher();
-            // Teacher second = new Teacher();
-            // Teacher third = new Teacher();
-
-            // double average = (first.Grade() + second.Grade() + third.Grade()) / 3.0;
-
-            // Console.WriteLine("Grading average " + average);
-
-
-            int hiiri = 5;
-            int keyboard = 10;
-
-            int tuotteidenHinta = Calc(hiiri, keyboard);
-            int postikulut = 10;
-
-            int total = Calc(tuotteidenHinta, postikulut);
-
-            // System.Console.WriteLine(total); //25
-
-
-            Calc(2, 2, 2);
+          break;
         }
-        private static int Calc(int a, int b)
-        {
-            int sum = a + b;
-            return sum;
-        }
+        string[] pieces = input.Split(",");
+        Console.WriteLine("nimi: " + pieces[0] + ", age: " + pieces[1]);
 
-        private static void Calc(int a, int b, int c)
-        {
-            int sum = a + b + c;
-            int sum2 = Calc(3, 3);
-            int total = sum + sum2;
-            System.Console.WriteLine(total.ToString());
-        }
+      }
+      Console.WriteLine("****************************************");
+      Console.WriteLine();
+      Console.WriteLine("****************************************");
+
+      string[] lines2 = File.ReadAllLines("records.csv");
+      foreach (string line in lines2)
+      {
+        Console.WriteLine("nimi: " + lines2[0] + " age:" + lines2[1]);
+      }
+      Console.WriteLine("****************************************");
+      Console.WriteLine();
+      Console.WriteLine("****************************************");
+
+
+      List<Person> people = new List<Person>();
+
+      string[] lines3 = File.ReadAllLines("records.csv");
+      foreach (string line in lines3)
+      {
+        string[] pieces = line.Split(",");
+        string name = pieces[0];
+        int age = Convert.ToInt32(pieces[1]);
+
+        people.Add(new Person(name, age));
+      }
+      Console.WriteLine("Total amount read: " + people.Count);
+
     }
+  }
 }
 
