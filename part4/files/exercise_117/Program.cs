8d28ce63b03e3c0124376825f2cec3dfdd47b657
Petri <petri.kivipuro@gmail.com>
2020-02-25 13:55:42 +0200

    Exercise 117 done

--- part4/files/exercise_117/Program.cs
+++ part4/files/exercise_117/Program.cs
@@ -8,8 +8,27 @@ namespace exercise_117
     public static void Main(string[] args)
     {
       // Ask the user for the file name and print the content of the file
+      while (true)
+      {
+        Console.WriteLine("Which file should have its contents printed?");
+        string content = Console.ReadLine();
+        /*
+        -- string file = File. ReadAllText(content);
+        -- Console. WriteLine(file);
+      JOS  käyttää näitä niin while looppia ja if komentoa ei tarvii
+     on itseasiassa parempi ja yksinkertaisempi niin
+    */
 
-      
+
+        if (content.Contains(content))
+        {
+          string text = File.ReadAllText(content);
+          Console.WriteLine(text);
+          break;
+        }
+      }
+
     }
+
   }
 }
