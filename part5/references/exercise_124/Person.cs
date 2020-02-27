83079c0c6f963b2e4dfcf8bd77c6cf684233553a
Petri <petri.kivipuro@gmail.com>
2020-02-27 14:46:17 +0200

    Exercise 124 done

---
+++ part5/references/exercise_124/Person.cs
@@ -0,0 +1,18 @@
+namespace exercise_124
+{
+  class Person
+  {
+    public string name;
+
+    public Person(string name)
+    {
+      this.name = name;
+    }
+
+
+    public void GrowOlder()
+    {
+      this.GrowOlder();
+    }
+  }
+}
