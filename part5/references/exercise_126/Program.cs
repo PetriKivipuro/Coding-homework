c402b8928e0e3a22c50ff4ae220dce71ed8863df
Petri <petri.kivipuro@gmail.com>
2020-02-28 16:18:09 +0200

    Exercise 126 done

--- part5/references/exercise_126/Program.cs
+++ part5/references/exercise_126/Program.cs
@@ -7,28 +7,22 @@ namespace exercise_126
         public static void Main(string[] args)
         {
             // Try your code here, if you want
-            PaymentCard petesCard = new PaymentCard(10);
-
-            Console.WriteLine("money " + petesCard.balance);
-            bool wasSuccessful = petesCard.TakeMoney(8);
-            Console.WriteLine("successfully withdrew: " + wasSuccessful);
-            Console.WriteLine("money " + petesCard.balance);
-
-            wasSuccessful = petesCard.TakeMoney(4);
-            Console.WriteLine("successfully withdrew: " + wasSuccessful);
-            Console.WriteLine("money " + petesCard.balance);
-
-            Console.WriteLine("******************************************");
             PaymentTerminal lunchCafeteria = new PaymentTerminal();
+            Console.WriteLine(lunchCafeteria);
 
-            double change = lunchCafeteria.DrinkCoffee(10);
-            Console.WriteLine("remaining change " + change);
+            PaymentCard annesCard = new PaymentCard(2);
 
-            change = lunchCafeteria.DrinkCoffee(5);
-            Console.WriteLine("remaining change " + change);
+            Console.WriteLine("amount of money on the card is " + annesCard.balance + " euros");
 
-            change = lunchCafeteria.EatLunch(20);
-            Console.WriteLine("remaining change " + change);
+            bool wasSuccessful = lunchCafeteria.EatLunch(annesCard);
+            Console.WriteLine("there was enough money: " + wasSuccessful);
+
+            lunchCafeteria.AddMoneyToCard(annesCard, 100);
+
+            wasSuccessful = lunchCafeteria.EatLunch(annesCard);
+            Console.WriteLine("there was enough money: " + wasSuccessful);
+
+            Console.WriteLine("amount of money on the card is " + annesCard.balance + " euros");
 
             Console.WriteLine(lunchCafeteria);
 
