396b2b5c685a8fe84b84fac6412be4ce4a98b599
Petri <petri.kivipuro@gmail.com>
2020-02-29 14:02:58 +0200

    Exercise 130 done

--- part5/references/exercise_130/Book.cs
+++ part5/references/exercise_130/Book.cs
@@ -1,28 +1,41 @@
 namespace exercise_130
 {
-
-
-  public class Book
-  {
-
-    private string name;
-    private int publicationYear;
-
-    public Book(string name, int publicationYear)
-    {
-      this.name = name;
-      this.publicationYear = publicationYear;
-    }
-
-
-    // BEGIN SOLUTION
-    public override bool Equals(object compared)
+    public class Book
     {
 
-      return false;
-    }
-    // END SOLUTION
-  }
+        private string name;
+        private int publicationYear;
 
+        public Book(string name, int publicationYear)
+        {
+            this.name = name;
+            this.publicationYear = publicationYear;
+        }
+
+
+        // BEGIN SOLUTION
+        public override bool Equals(object compared)
+        {
+            if (this == compared)
+            {
+                return true;
+            }
+            if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+            {
+                return false;
+            }
+
+            Book comparedBooks = (Book)compared;
+
+            if (this.name == comparedBooks.name &&
+                this.publicationYear == comparedBooks.publicationYear)
+            {
+                return true;
+            }
+            return false;
+
+            // END SOLUTION
+        }
+    }
 
 }
