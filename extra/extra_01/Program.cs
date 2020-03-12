112182f580c40da6a4d64f85a05fd9e48b9b2350
Petri <petri.kivipuro@gmail.com>
2020-03-12 11:53:30 +0200

    extra1 done

--- extra/extra_01/Program.cs
+++ extra/extra_01/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace extra_01
 {
@@ -7,7 +9,28 @@ namespace extra_01
     public static void Main(string[] args)
     {
       // Add your code here:
+      Console.WriteLine("What do you want to print?");
+
+      string sentence = Console.ReadLine();
+
+      Console.WriteLine("How many times you want to print?");
+      int amount = Convert.ToInt32(Console.ReadLine());
+
+
+      for (int i = 0; i < amount; i++)
+      {
+        Console.WriteLine(sentence);
+      }
+
+      /* int i = 0;
+       while (i < amount)
+       {
+         Console. WriteLine(sentence);
+         i++;
+       } */
 
     }
+
   }
 }
+
