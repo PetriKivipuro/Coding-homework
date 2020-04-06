2b7eb09d98a5e92e1421018b484e0c5b8f975984
Petri <petri.kivipuro@gmail.com>
2020-04-06 10:21:06 +0300

    tehtävä kesken

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -2,22 +2,22 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class Abbreviations
-  {
-
-    public void AddAbbreviation(string abbreviation, string explanation)
+    public class Abbreviations
     {
+        private Dictionary<string, string> kirjasto;
+        public void AddAbbreviation(string abbreviation, string explanation)
+        {
+            this.kirjasto = new Dictionary<string, string>();
+        }
 
-    }
+        public bool HasAbbreviation(string abbreviation)
+        {
+            return false;
+        }
 
-    public bool HasAbbreviation(string abbreviation)
-    {
-      return false;
+        public string FindExplanationFor(string abbreviation)
+        {
+            return null;
+        }
     }
-
-    public string FindExplanationFor(string abbreviation)
-    {
-      return null;
-    }
-  }
 }
