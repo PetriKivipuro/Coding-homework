89c2d13231d406f94bfd545f17ce423c9acdc9d7
Petri <petri.kivipuro@gmail.com>
2020-04-06 14:41:16 +0300

    Sand2 push

---
+++ Rojektikansio/sand2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello ebo");
+
+
+        }
+
+    }
+}
