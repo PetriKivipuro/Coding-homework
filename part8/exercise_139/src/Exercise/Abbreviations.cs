309f343a99a31fe7a924a17099226d34d0420526
Petri <petri.kivipuro@gmail.com>
2020-04-07 19:41:59 +0300

    useless push 139

--- part8/exercise_139/src/Exercise/Abbreviations.cs
+++ part8/exercise_139/src/Exercise/Abbreviations.cs
@@ -4,20 +4,35 @@ namespace Exercise
 {
     public class Abbreviations
     {
-        private Dictionary<string, string> kirjasto;
+        private Dictionary<string, string> abbreviations;
+
+        public Abbreviations()
+        {
+            this.abbreviations = new Dictionary<string, string>();
+        }
+
         public void AddAbbreviation(string abbreviation, string explanation)
         {
-            this.kirjasto = new Dictionary<string, string>();
+            this.abbreviations.Add(abbreviation, explanation);
         }
 
         public bool HasAbbreviation(string abbreviation)
         {
+            if (this.abbreviations.ContainsKey(abbreviation))
+            {
+                return true;
+            }
             return false;
         }
 
+
         public string FindExplanationFor(string abbreviation)
         {
-            return null;
+            if (HasAbbreviation(abbreviation))
+            {
+                return this.abbreviations[abbreviation];
+            }
+            return "Not found!";
         }
     }
 }
