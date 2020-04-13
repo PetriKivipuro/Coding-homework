ed549476fa58975e1fa3a70c1ff8068d5996d352
Petri <petri.kivipuro@gmail.com>
2020-04-13 15:07:53 +0300

    141 asdf

--- part8/exercise_141/src/Exercise/Program.cs
+++ part8/exercise_141/src/Exercise/Program.cs
@@ -3,29 +3,37 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
-      Dictionary<string, Book> books = new Dictionary<string, Book>();
-      Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
-      Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
-      books.Add(senseAndSensibility.name, senseAndSensibility);
-      books.Add(prideAndPrejudice.name, prideAndPrejudice);
+        public static void Main(string[] args)
+        {
+            Dictionary<string, Book> books = new Dictionary<string, Book>();
+            Book senseAndSensibility = new Book("Sense and Sensibility", 1811, "...");
+            Book prideAndPrejudice = new Book("Pride and Prejudice", 1813, "....");
+            books.Add(senseAndSensibility.name, senseAndSensibility);
+            books.Add(prideAndPrejudice.name, prideAndPrejudice);
 
-      PrintValues(books);
-      Console.WriteLine("-- -- -- --");
-      PrintValueIfNameContains(books, "prejud");
+            PrintValues(books);
+            Console.WriteLine("-- -- -- --");
+            PrintValueIfNameContains(books, "prejud");
+        }
+
+        public static void PrintValues(Dictionary<string, Book> dictionary)
+        {
+            foreach (KeyValuePair<string, Book> kvp in dictionary)
+            {
+                Console.WriteLine("{0}", kvp.Value);
+            }
+        }
+        public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
+        {
+            foreach (KeyValuePair<string, Book> kvp in dictionary)
+            {
+                if (kvp.Value.name.ToLower().Trim().Contains(text))
+                {
+                    Console.WriteLine("{0}", kvp.Value);
+                }
+            }
+        }
     }
-
-    public static void PrintValues(Dictionary<string, Book> dictionary)
-    {
-
-    }
-    public static void PrintValueIfNameContains(Dictionary<string, Book> dictionary, string text)
-    {
-
-    }
-  }
 }
-
