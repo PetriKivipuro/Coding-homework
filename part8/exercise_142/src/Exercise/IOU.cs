859c204522f0d202b967557f6c34375fbdb93943
Petri <petri.kivipuro@gmail.com>
2020-04-14 23:21:30 +0300

    oushhh

--- part8/exercise_142/src/Exercise/IOU.cs
+++ part8/exercise_142/src/Exercise/IOU.cs
@@ -1,17 +1,53 @@
-
+using System;
+using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class IOU
-  {
-    public void ChangeDebt(string toWhom, int amount)
+    public class IOU
     {
 
-    }
+        private Dictionary<string, int> velka;
 
-    public int HowMuchDoIOweTo(string toWhom)
-    {
-      return 0;
+        public IOU()
+        {
+            this.velka = new Dictionary<string, int>();
+        }
+
+
+        public void ChangeDebt(string toWhom, int amount)
+        {
+            if (this.velka.ContainsKey(toWhom))
+            {
+                if (velka[toWhom] + amount >= 0)
+                {
+                    velka[toWhom] += amount;
+                }
+                else
+                {
+                    velka[toWhom] = 0;
+                }
+            }
+            else if (amount >= 0)
+            {
+                velka.Add(toWhom, amount);
+            }
+            else
+            {
+                velka.Add(toWhom, 0);
+            }
+        }
+
+
+        public int HowMuchDoIOweTo(string toWhom)
+        {
+            if (this.velka.ContainsKey(toWhom))
+            {
+                return this.velka[toWhom];
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
-  }
 }
--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -9,15 +9,51 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
-            TaskTracker trak = new TaskTracker();
+            Dictionary<string, string> postalCodes = new Dictionary<string, string>();
+            postalCodes.Add("07100", "Helsinki");
+            postalCodes.Add("90014", "Oulu");
+            postalCodes.Add("33720", "Tampere");
+            postalCodes.Add("33014", null);
 
-            trak.Add("ada", 5);
-            trak.Add("ada2", 6);
-            trak.Add("ada3", 58);
-            trak.Add("ada4", 45);
-            trak.Add("pekka", 100);
+            Console.WriteLine(postalCodes["33720"]);
 
-            trak.Print();
+            if (!postalCodes.ContainsKey("07100"))
+            {
+                postalCodes.Add("07100", "Hesa");
+            }
+
+            Dictionary<int, string> hlotns = new Dictionary<int, string>();
+            hlotns.Add(291186, "Pete");
+            hlotns.Add(060623, "Juki");
+
+            Console.WriteLine(hlotns[291186] + " " + postalCodes["07100"]);
+
+            foreach (KeyValuePair<string, string> kvp in postalCodes)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+
+            }
+
+            foreach (KeyValuePair<int, string> kvp in hlotns)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+            Console.WriteLine("---------------------------------------------");
+
+
+
+            Book senseAndSoOn = new Book("Sense ans soon", 500, "jajajajaj");
+            Book prideAndShit = new Book("Pride and Shit", 850, "wolwolwolololoo");
+
+            Library books = new Library();
+            books.AddBook(senseAndSoOn);
+            books.AddBook(prideAndShit);
+
+            Console.WriteLine(books.GetBook("Sense ans soon"));
+
+            Console.WriteLine(books.GetBook("SENSE ANS SOON"));
+
+            Console.WriteLine(books.GetBook("SENSE"));
 
 
 
