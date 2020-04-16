d3bdfad26235e248600c54ef922ee1b771b8e41c
Petri <petri.kivipuro@gmail.com>
2020-04-16 16:15:26 +0300

    ljkasf

--- part8/exercise_145/src/Exercise/LicensePlate.cs
+++ part8/exercise_145/src/Exercise/LicensePlate.cs
@@ -1,30 +1,43 @@
 namespace Exercise
 {
-  public class LicensePlate
-  {
-    public string liNumber { get; }
-    private string country;
-
-    public LicensePlate(string country, string liNumber)
+    public class LicensePlate
     {
-      this.liNumber = liNumber;
-      this.country = country;
-    }
+        public string liNumber { get; }
+        private string country;
+
+        public LicensePlate(string country, string liNumber)
+        {
+            this.liNumber = liNumber;
+            this.country = country;
+        }
 
 
-    public override string ToString()
-    {
-      return country + " " + liNumber;
-    }
+        public override string ToString()
+        {
+            return country + " " + liNumber;
+        }
 
-    public override bool Equals(object compared)
-    {
-      return false;
-    }
+        public override bool Equals(object compared)
+        {
+            if (this == compared)
+            {
+                return true;
+            }
 
-    public override int GetHashCode()
-    {
-      return -1;
+            if ((compared == null) || !this.GetType().Equals(compared.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                LicensePlate compLice = (LicensePlate)compared;
+                return this.country == compLice.country && this.liNumber == compLice.liNumber;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.liNumber.GetHashCode();
+        }
     }
-  }
 }
--- part8/exercise_145/src/Exercise/VehicleRegistry.cs
+++ part8/exercise_145/src/Exercise/VehicleRegistry.cs
@@ -1,34 +1,77 @@
 using System;
 using System.Collections.Generic;
 
+
 namespace Exercise
 {
-  public class VehicleRegistry
-  {
-    private Dictionary<LicensePlate, string> owners = new Dictionary<LicensePlate, string>();
-    public bool Add(LicensePlate licensePlate, string owner)
+    public class VehicleRegistry
     {
-      return false;
-    }
+        private Dictionary<LicensePlate, string> owners = new Dictionary<LicensePlate, string>();
+        public bool Add(LicensePlate licensePlate, string owner)
+        {
 
-    public string Get(LicensePlate licensePlate)
-    {
-      return "No such license";
-    }
+            if (!this.owners.ContainsKey(licensePlate))
+            {
+                this.owners.Add(licensePlate, owner);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-    public bool Remove(LicensePlate licensePlate)
-    {
-      return false;
-    }
+        public string Get(LicensePlate licensePlate)
+        {
+            if (this.owners.ContainsKey(licensePlate))
+            {
+                return this.owners[licensePlate];
+            }
+            else
+            {
+                return "No such license";
+            }
+        }
 
-    public void PrintLicensePlates()
-    {
-      
-    }
+        public bool Remove(LicensePlate licensePlate)
+        {
+            if (this.owners.ContainsKey(licensePlate))
+            {
+                this.owners.Remove(licensePlate);
+                return true;
+            }
 
-    public void PrintOwners()
-    {
+            else
+            {
 
+                Console.WriteLine("ei voitu poistaa");
+                return false;
+
+            }
+        }
+
+        public void PrintLicensePlates()
+        {
+            foreach (KeyValuePair<LicensePlate, string> kvpLisPla in owners)
+            {
+                Console.WriteLine("{0}", kvpLisPla.Key);
+
+            }
+        }
+
+        public void PrintOwners()
+        {
+            List<string> listOfOwners = new List<string>();
+
+            foreach (KeyValuePair<LicensePlate, string> kvpowner in this.owners)
+            {
+                if (!listOfOwners.Contains(kvpowner.Value))
+                {
+                    Console.WriteLine(kvpowner.Value);
+                }
+                listOfOwners.Add(kvpowner.Value);
+            }
+
+        }
     }
-  }
 }
