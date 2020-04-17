9a6e30c744763a5366f4ffa9fd7bcb08cbdb7f72
Petri <petri.kivipuro@gmail.com>
2020-04-17 12:24:39 +0300

    147 blabla ctrlcv

--- part8/exercise_147/src/Exercise/StorageFacility.cs
+++ part8/exercise_147/src/Exercise/StorageFacility.cs
@@ -3,26 +3,53 @@ using System;
 
 namespace Exercise
 {
-  public class StorageFacility
-  {
-
-    public void Add(string unit, string item)
+    public class StorageFacility
     {
 
-    }
+        private Dictionary<string, List<string>> varasto = new Dictionary<string, List<string>>();
+        public void Add(string unit, string item)
+        {
+            if (!this.varasto.ContainsKey(unit))
+            {
+                this.varasto.Add(unit, new List<string> { item });
+            }
+            else
+            {
+                this.varasto[unit].Add(item);
+            }
+        }
 
-    public List<string> Contents(string storageUnit)
-    {
-      return new List<string>();
-    }
+        public List<string> Contents(string storageUnit)
+        {
+            if (!this.varasto.ContainsKey(storageUnit))
+            {
+                return this.varasto[storageUnit];
+            }
+            return this.varasto[storageUnit];
+        }
 
-    public void Remove(string storageUnit, string item)
-    {
+        public void Remove(string storageUnit, string item)
+        {
+            if (this.varasto.ContainsKey(storageUnit))
+            {
+                this.varasto[storageUnit].Remove(item);
+            }
+            if (this.varasto[storageUnit].Count == 0)
+            {
+                this.varasto.Remove(storageUnit);
+            }
+        }
 
-    }
+        public List<string> StorageUnits()
+        {
+            List<string> varastoLista = new List<string>();
+            Dictionary<string, List<string>>.KeyCollection keys = this.varasto.Keys;
 
-    public List<string> StorageUnits()
-    {
-      return new List<string>();
+            foreach (string yksikonNimi in keys)
+            {
+                varastoLista.Add(yksikonNimi);
+            }
+            return varastoLista;
+        }
     }
-  }
+}
