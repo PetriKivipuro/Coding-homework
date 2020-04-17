cf74984c8ed12348ca9747a3f73652bf8086a79a
Petri <petri.kivipuro@gmail.com>
2020-04-17 10:54:11 +0300

    pushing

--- part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
+++ part8/exercise_146/src/Exercise/DictionaryOfManyTranslations.cs
@@ -2,22 +2,35 @@ using System.Collections.Generic;
 
 namespace Exercise
 {
-  public class DictionaryOfManyTranslations
-  {
-
-    public void Add(string word, string translation)
+    public class DictionaryOfManyTranslations
     {
+        private Dictionary<string, List<string>> sanakirja;
+        public DictionaryOfManyTranslations()
+        {
+            this.sanakirja = new Dictionary<string, List<string>>();
+        }
+        public void Add(string word, string translation)
+        {
+            if (!this.sanakirja.ContainsKey(word))
+            {
+                this.sanakirja.Add(word, new List<string>());
+            }
+            List<string> sanakirjaValmis = this.sanakirja[word];
+            sanakirjaValmis.Add(translation);
+        }
 
+        public List<string> Translate(string word)
+        {
+            if (!this.sanakirja.ContainsKey(word))
+            {
+                this.sanakirja.Add(word, new List<string>());
+            }
+            return this.sanakirja[word];
+        }
+
+        public void Remove(string word)
+        {
+            this.sanakirja.Remove(word);
+        }
     }
-
-    public List<string> Translate(string word)
-    {
-      return new List<string>();
-    }
-
-    public void Remove(string word)
-    {
-
-    }
-  }
 }
