5ec3cb7c6cfc20a7c5ea94d9c575495f6fae4f8a
Petri <petri.kivipuro@gmail.com>
2020-04-19 15:09:38 +0300

    149 moving on allmost success

--- part9/exercise_149/src/Exercise/People/Person.cs
+++ part9/exercise_149/src/Exercise/People/Person.cs
@@ -1,6 +1,21 @@
 namespace Exercise
 {
-  public class Person
-  {
-  }
+    public class Person
+    {
+        public string name { get; }
+        public string address { get; }
+
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public override string ToString()
+        {
+            return this.name + ", " + this.address;
+        }
+
+
+
+    }
 }
--- part9/exercise_149/src/Exercise/Program.cs
+++ part9/exercise_149/src/Exercise/Program.cs
@@ -2,11 +2,28 @@ using System;
 
 namespace Exercise
 {
-  public class Program
-  {
-    public static void Main(string[] args)
+    public class Program
     {
+        public static void Main(string[] args)
+        {
+            Person ada = new Person("Ada Lovelace", "24 Maddox St. London W1S 2QN");
+            Person esko = new Person("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki");
+            Console.WriteLine(ada);
+            Console.WriteLine(esko);
 
+            Console.WriteLine();
+
+            Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+            Console.WriteLine(ollie);
+            ollie.Study();
+            Console.WriteLine(ollie);
+
+            Console.WriteLine();
+
+            Teacher ada1 = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+            Teacher esko1 = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+            Console.WriteLine(ada1);
+            Console.WriteLine(esko1);
+        }
     }
-  }
 }
