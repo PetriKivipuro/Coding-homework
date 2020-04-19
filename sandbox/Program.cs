e6e21e47a9498f5357264370c24b468c65c1fa83
Petri <petri.kivipuro@gmail.com>
2020-04-19 13:58:45 +0300

    sandis

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -9,52 +9,13 @@ namespace sandbox
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> postalCodes = new Dictionary<string, string>();
-            postalCodes.Add("07100", "Helsinki");
-            postalCodes.Add("90014", "Oulu");
-            postalCodes.Add("33720", "Tampere");
-            postalCodes.Add("33014", null);
 
-            Console.WriteLine(postalCodes["33720"]);
+            Engine moottori = new Engine("bensa", "miksi tyhjä", "Pösö", "Pesöö 3L 99-01");
 
-            if (!postalCodes.ContainsKey("07100"))
-            {
-                postalCodes.Add("07100", "Hesa");
-            }
-
-            Dictionary<int, string> hlotns = new Dictionary<int, string>();
-            hlotns.Add(291186, "Pete");
-            hlotns.Add(060623, "Juki");
-
-            Console.WriteLine(hlotns[291186] + " " + postalCodes["07100"]);
-
-            foreach (KeyValuePair<string, string> kvp in postalCodes)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-
-            }
-
-            foreach (KeyValuePair<int, string> kvp in hlotns)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-            Console.WriteLine("---------------------------------------------");
-
-
-
-            Book senseAndSoOn = new Book("Sense ans soon", 500, "jajajajaj");
-            Book prideAndShit = new Book("Pride and Shit", 850, "wolwolwolololoo");
-
-            Library books = new Library();
-            books.AddBook(senseAndSoOn);
-            books.AddBook(prideAndShit);
-
-            Console.WriteLine(books.GetBook("Sense ans soon"));
-
-            Console.WriteLine(books.GetBook("SENSE ANS SOON"));
-
-            Console.WriteLine(books.GetBook("SENSE"));
-
+            Console.WriteLine(moottori.engineType);
+            Console.WriteLine(moottori.identifier);
+            Console.WriteLine(moottori.manufacturer);
+            Console.WriteLine(moottori.description);
 
 
         }
