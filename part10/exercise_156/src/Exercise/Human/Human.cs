c9ddee8ded882b3b43b7dac622207cb16a77346e
Petri <petri.kivipuro@gmail.com>
2020-04-26 18:01:50 +0300

    156

--- part10/exercise_156/src/Exercise/Human/Human.cs
+++ part10/exercise_156/src/Exercise/Human/Human.cs
@@ -1,30 +1,30 @@
 namespace Exercise
 {
-  using System;
-  public class Human : IComparable<Human>
-  {
-
-    public int wage { get; }
-    public string name { get; }
-
-    public Human(string name, int wage)
+    using System;
+    public class Human : IComparable<Human>
     {
-      this.name = name;
-      this.wage = wage;
-    }
 
-    public int CompareTo(Human another)
-    {
-      // BEGIN SOLUTION
-      return 0;
-      // END SOLUTION
-    }
-    
+        public int wage { get; }
+        public string name { get; }
+
+        public Human(string name, int wage)
+        {
+            this.name = name;
+            this.wage = wage;
+        }
+
+        public int CompareTo(Human another)
+        {
+            // BEGIN SOLUTION
+            return another.wage - this.wage;
+            // END SOLUTION
+        }
 
 
-    public override string ToString()
-    {
-      return name + " " + wage;
+
+        public override string ToString()
+        {
+            return name + " " + wage;
+        }
     }
-  }
 }
