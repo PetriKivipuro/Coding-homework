37113e109fac71614b3db803bc652b809b2e875b
Petri <petri.kivipuro@gmail.com>
2020-04-27 19:47:24 +0300

    160-

--- part10/exercise_160/src/Exercise/CardGame/Card.cs
+++ part10/exercise_160/src/Exercise/CardGame/Card.cs
@@ -1,25 +1,48 @@
 namespace Exercise
 {
-  using System;
-  public class Card : IComparable<Card>
-  {
-    public int value { get; }
-    public Suit suit { get; }
-
-    public Card(int value, Suit suit)
+    using System;
+    public class Card : IComparable<Card>
     {
-      this.value = value;
-      this.suit = suit;
-    }
+        public int value { get; }
+        public Suit suit { get; }
 
-    public override string ToString()
-    {
-      return "";
-    }
+        public Card(int value, Suit suit)
+        {
+            this.value = value;
+            this.suit = suit;
+        }
 
-    public int CompareTo(Card another)
-    {
-      return 0;
+        public override string ToString()
+        {
+            if (this.value == 14)
+            {
+                return this.suit + " A";
+            }
+            if (this.value == 13)
+            {
+                return this.suit + " K";
+            }
+            if (this.value == 12)
+            {
+                return this.suit + " Q";
+            }
+
+            if (this.value == 11)
+            {
+                return this.suit + " J";
+            }
+
+            return this.suit + " " + this.value;
+        }
+
+        public int CompareTo(Card another)
+        {
+            if (this.value == another.value)
+            {
+                return this.suit - another.suit;
+            }
+
+            return this.value - another.value;
+        }
     }
-  }
 }
--- part10/exercise_160/src/Exercise/CardGame/Hand.cs
+++ part10/exercise_160/src/Exercise/CardGame/Hand.cs
@@ -1,27 +1,65 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class Hand : IComparable<Hand>
-  {
-    private List<Card> hand;
-
-    public void Add(Card card)
+    using System;
+    using System.Collections.Generic;
+    public class Hand : IComparable<Hand>
     {
-    }
+        private List<Card> hand;
+        public Hand()
+        {
+            this.hand = new List<Card>();
+        }
+        public void Add(Card card)
+        {
+            if (this.hand.Contains(card))
+            {
+                return;
+            }
+            else
+            {
+                this.hand.Add(card);
+            }
+        }
 
-    public void Print()
-    {
-    }
+        public void Print()
+        {
+            foreach (Card kortit in this.hand)
+            {
+                Console.WriteLine(kortit.ToString());
+            }
+        }
 
-    public void Sort()
-    {
-    }
+        public void Sort()
+        {
+            this.hand.Sort();
+        }
 
 
-    public int CompareTo(Hand hand)
-    {
-      return 0;
+        public int CompareTo(Hand hand)
+        {
+            int sum = 0;
+            foreach (Card cards in this.hand)
+            {
+                sum += cards.value;
+            }
+            int sum2 = 0;
+            foreach (Card cards in hand.hand)
+            {
+                sum2 += cards.value;
+            }
+            if (sum == sum2)
+            {
+                return 0;
+            }
+            if (sum < sum2)
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+
+        }
     }
-  }
 }
