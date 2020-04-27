d4fac32c5d310d68f1342a951f50a65d71013644
Petri <petri.kivipuro@gmail.com>
2020-04-27 13:41:17 +0300

    bush 158

--- part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
+++ part10/exercise_158/src/Exercise/UserInterfaces/TextInterface.cs
@@ -1,17 +1,45 @@
 namespace Exercise
 {
-  using System;
-  using System.Collections.Generic;
-  public class TextInterface
-  {
-    // Create the userinterface here
-    private List<Book> books;
-    public TextInterface()
+    using System;
+    using System.Collections.Generic;
+    public class TextInterface
     {
-    }
+        // Create the userinterface here
 
-    public void Start()
-    {
+        private List<Book> books;
+        public TextInterface()
+        {
+            this.books = new List<Book>();
+        }
+
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input the name of the book, empty stops: ");
+                string nameBook = Console.ReadLine();
+
+                if (nameBook == "")
+                {
+                    break;
+                }
+                Console.WriteLine("Input the age recommendation: ");
+                string ageReco = Console.ReadLine();
+
+                int age = Convert.ToInt32(ageReco);
+                this.books.Add(new Book(nameBook, age));
+
+            }
+            Console.WriteLine(this.books.Count + " books in total." + "\n");
+
+            Console.WriteLine("Books:");
+
+            books.Sort();
+            foreach (Book kirjaset in this.books)
+            {
+                Console.WriteLine(kirjaset);
+            }
+
+        }
     }
-  }
 }
