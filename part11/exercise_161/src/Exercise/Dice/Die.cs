aedf5bf681be8bb4aed2b2a6418bcfc58be703bb
Petri <petri.kivipuro@gmail.com>
2020-04-30 14:29:20 +0300

    5

--- part11/exercise_161/src/Exercise/Dice/Die.cs
+++ part11/exercise_161/src/Exercise/Dice/Die.cs
@@ -1,6 +1,5 @@
 namespace Exercise
 {
-<<<<<<< HEAD
     using System;
     public class Die
     {
@@ -21,29 +20,5 @@ namespace Exercise
             return diceFace;
 
         }
-=======
-  using System;
-  public class Die
-  {
-    private Random random;
-    private int numberOfFaces;
-
-    public Die(int numberOfFaces)
-    {
-      this.random = new Random();
-      // Initialize the value of numberOfFaces here
-      this.numberOfFaces = numberOfFaces;
-    }
-    public int ThrowDie()
-    {
-      // generate a random number which may be any number
-      // between one and the number of faces, and then return it
-      int cast = random.Next(1, this.numberOfFaces + 1);
-
-      // numberOfFaces = 6
-      // Next(1,6)
-      // between 1 and 5
-      return cast;
->>>>>>> c41c326b51267b8a870cd7add7774b5bfb1e0b12
     }
 }
