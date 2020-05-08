bfe04c6424d9bfc4050c5ac9ba4a27942e0762cf
Petri <petri.kivipuro@gmail.com>
2020-05-08 12:00:13 +0300

    167 done

--- part12/exercise_167/src/Exercise/Program.cs
+++ part12/exercise_167/src/Exercise/Program.cs
@@ -1,37 +1,44 @@
 namespace Exercise
 {
-  // using something...
+    // using something...
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
 
-  public class Program
-  {
-    public static void Main(string[] args)
+
+    public class Program
     {
+        public static void Main(string[] args)
+        {
 
-      List<int> integerList = new List<int>();
-      List<string> stringList = new List<string>();
+            List<int> integerList = new List<int>();
+            List<string> stringList = new List<string>();
 
-      string str = "This needs a using!";
-      string another = "This does not need using!";
-      stringList.Add(str);
-      stringList.Add(another);
+            string str = "This needs a using!";
+            string another = "This does not need using!";
+            stringList.Add(str);
+            stringList.Add(another);
 
-      Regex rgx = new Regex("needs");
+            Regex rgx = new Regex("needs");
 
-      foreach (string sentence in stringList) {
-        if (rgx.IsMatch(sentence)) {
-          Console.WriteLine(sentence);
+            foreach (string sentence in stringList)
+            {
+                if (rgx.IsMatch(sentence))
+                {
+                    Console.WriteLine(sentence);
+                }
+            }
+
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                integerList.Add(rnd.Next(1, 10001));
+            }
+            int largest = integerList.Max();
+            int smallest = integerList.Min();
+            Console.WriteLine(largest);
+            Console.WriteLine(smallest);
         }
-      }
-
-      Random rnd = new Random();
-      for (int i = 0; i < 1000; i++)
-      {
-        integerList.Add(rnd.Next(1, 10001));
-      }
-      int largest = integerList.Max();
-      int smallest = integerList.Min();
-      Console.WriteLine(largest);
-      Console.WriteLine(smallest);
     }
-  }
 }
